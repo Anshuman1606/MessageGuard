9839bd66a54f737b379760f709625d840029bb05
Anshuman1606 <alfahunt61@gmail.com>
2026-07-04 01:57:46 +0530

    updates

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pickle
 import nltk
 import string
+nltk.download("punkt")
+nltk.download("stopwords")
 
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
